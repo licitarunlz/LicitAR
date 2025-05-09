344934ce34f2857de3cafeeb1415069090a3683c
Pablo Goitisolo <pablo.numb@gmail.com>
2025-05-09 18:17:11 -0300

    [Fix] Primary Key

--- LicitAR.Core/Data/Models/EntidadLicitanteUsuario.cs
+++ LicitAR.Core/Data/Models/EntidadLicitanteUsuario.cs
@@ -1,15 +1,25 @@
 using LicitAR.Core.Data.Models.Helpers;
 using LicitAR.Core.Data.Models.Identidad; // Add this line to resolve LicitArUser
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace LicitAR.Core.Data.Models
 {
+    [PrimaryKey(nameof(IdEntidadLicitante), nameof(IdUsuario))]
     public class EntidadLicitanteUsuario
     {
+        [Required(ErrorMessage = ErrorMessages.REQUIRED)]
         public int IdEntidadLicitante { get; set; }
+
+        [Required(ErrorMessage = ErrorMessages.REQUIRED)]
         public EntidadLicitante EntidadLicitante { get; set; }
 
+        [Required(ErrorMessage = ErrorMessages.REQUIRED)]
+        [MaxLength(50, ErrorMessage = ErrorMessages.MAXLENGTH)]
         public string IdUsuario { get; set; }
+
+        [Required(ErrorMessage = ErrorMessages.REQUIRED)]
         public LicitArUser Usuario { get; set; }
 
         [NotMapped]
