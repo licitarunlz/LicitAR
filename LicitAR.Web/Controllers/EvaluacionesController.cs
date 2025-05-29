cac3cc74c42c9492856e4a2ccf3d7a28dda1c9a9
Diego Peppert <diego@80grados.com.ar>
2025-05-29 00:22:15 -0300

    [Add] se agrega el detalle pero aun no se marcan los tildados

--- LicitAR.Web/Controllers/EvaluacionesController.cs
+++ LicitAR.Web/Controllers/EvaluacionesController.cs
@@ -10,6 +10,8 @@ using LicitAR.Core.Data.Models;
 using LicitAR.Web.Helpers;
 using LicitAR.Web.Models;
 using LicitAR.Core.Business.Licitaciones;
+using LicitAR.Core.Data.Models.Helpers;
+using LicitAR.Core.Utils;
 
 namespace LicitAR.Web.Controllers
 {
@@ -18,12 +20,17 @@ namespace LicitAR.Web.Controllers
         private LicitARDbContext _context;
         private ILicitacionManager _licitacionManager;
         private IOfertaManager _ofertaManager;
+        private IEvaluacionManager _evaluacionManager;
 
-        public EvaluacionesController(LicitARDbContext context, ILicitacionManager licitacionManager, IOfertaManager ofertaManager)
+        public EvaluacionesController(LicitARDbContext context,
+                                      ILicitacionManager licitacionManager,
+                                      IOfertaManager ofertaManager,
+                                      IEvaluacionManager evaluacionManager)
         {
             _context = context;
             _licitacionManager = licitacionManager;
             _ofertaManager = ofertaManager;
+            _evaluacionManager = evaluacionManager;
         }
 
         // GET: Evaluaciones
@@ -36,21 +43,50 @@ namespace LicitAR.Web.Controllers
         // GET: Evaluaciones/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            /*  if (id == null)
-              {
-                  return NotFound();
-              }
 
-              var evaluacion = await _context.Evaluaciones
-                  .Include(e => e.Licitacion)
-                  .FirstOrDefaultAsync(m => m.IdEvaluacion == id);
-              if (evaluacion == null)
-              {
-                  return NotFound();
-              }
+            if (id == null)
+                return View("NotFound");
 
-              return View(evaluacion);*/
-            return View();
+            var evaluacion = await _evaluacionManager.GetEvaluacionByIdAsync(id.Value);
+
+            if (evaluacion == null)
+            {
+                return View("NotFound"); // Updated
+            }
+
+            var licitacion = await _licitacionManager.GetLicitacionByIdAsync(evaluacion.IdLicitacion);
+            if (licitacion == null)
+            {
+                return View("NotFound"); // Updated
+            }
+
+
+            licitacion.Items = licitacion.Items.Where(x => x.Audit.FechaBaja == null).ToList();
+
+            ViewBag.licitacion = licitacion;
+
+            var ofertas = await _ofertaManager.GetAllOfertasPorLicitacionAsync(evaluacion.IdLicitacion);
+
+            ViewBag.ofertas = ofertas;
+            EvaluacionModel model = new EvaluacionModel();
+            model.SetEvaluacion(evaluacion);
+            var ofertasGanadoras = evaluacion.EvaluacionOfertasDetalles.Select(x => x.IdOfertaDetalle);
+            var ofertasGanadorasModel = new Dictionary<int, int>();
+
+            foreach (var oferta in ofertas)
+            {
+                foreach (var ofertaDetalle in oferta.Items)
+                {
+
+                    if (ofertasGanadoras.Contains(ofertaDetalle.IdOfertaDetalle))
+                    {
+                        ofertasGanadorasModel.Add(ofertaDetalle.IdLicitacionDetalle, ofertaDetalle.IdOfertaDetalle);
+                    }
+                }
+            }
+            model.Ofertas = ofertasGanadorasModel;
+
+            return View(model);
         }
 
         // GET: Evaluaciones/Create
@@ -91,7 +127,12 @@ namespace LicitAR.Web.Controllers
         {
             if (ModelState.IsValid)
             {
+                AuditTable table = AuditHelper.GetCreationData(IdentityHelper.GetUserLicitARId(User));
+                Evaluacion eval = evaluacion.GetEvaluacion(table);
+                eval.EvaluacionOfertasDetalles = evaluacion.GetEvaluacionOferta(table).ToList();
 
+                await _evaluacionManager.CreateEvaluacionAsync(eval, IdentityHelper.GetUserLicitARId(User));
+                return RedirectToAction(nameof(Index));
                 /*_context.Add(evaluacion);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));*/
