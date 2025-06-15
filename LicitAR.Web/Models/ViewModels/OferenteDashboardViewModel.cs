18c82b7a9f10354e6886a2b636463f95ded13693
Pablo Goitisolo <pablo.numb@gmail.com>
2025-06-15 18:00:54 -0300

    [Fix] Paginado Audittrail

--- LicitAR.Web/Models/ViewModels/OferenteDashboardViewModel.cs
+++ LicitAR.Web/Models/ViewModels/OferenteDashboardViewModel.cs
@@ -5,6 +5,14 @@ namespace LicitAR.Web.ViewModels.Dashboard
         public int LicitacionesDisponibles { get; set; }
         public int LicitacionesEnCurso { get; set; }
         public int AdjudicacionesGanadas { get; set; }
-        // Agregar más propiedades según necesidad
+
+        public int TotalLicitaciones { get; set; }
+        public int TotalLicitacionesActivas { get; set; }
+        public int TotalAdjudicaciones { get; set; }
+        public int LicitacionesActivas { get; set; }
+        public int Adjudicaciones { get; set; }
+
+        public decimal PorcentajeLicitacionesActivasVsMesAnterior { get; set; }
+        public decimal PorcentajeAdjudicacionesVsMesAnterior { get; set; }
     }
 }
